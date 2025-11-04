738859bb1f7ed89030bdfc523ccb8bcdd4aba4f9
Damon <ddgardnerjr@gmail.com>
2025-11-04 01:04:30 -0500

    Final1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping",
+  base: "/https://github.com/damong31/e-plantShopping",
   plugins: [react()],
 })
